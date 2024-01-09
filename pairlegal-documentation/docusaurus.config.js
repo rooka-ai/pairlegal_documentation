1ae002c4f12f5c5b4aa2c1ebf6990295a2d4e840
Karim S <karimsehimi3s1@gmail.com>
2024-01-09 16:43:23 +0100

    feat: add search button on top

--- pairlegal-documentation/docusaurus.config.js
+++ pairlegal-documentation/docusaurus.config.js
@@ -4,84 +4,98 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import { themes as prismThemes } from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Rooka',
-  tagline: 'Legal Drafting. Redefined.',
-  favicon: 'img/logo.png',
+  title: "Rooka",
+  tagline: "Legal Drafting. Redefined.",
+  favicon: "img/logo.png",
 
   // Set the production url of your site here
-  url: 'https://rooka.ai/',
+
+  url: "https://rooka.ai/", //Shouldn't be https://docs.singledraft.ai
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'rooka-ai', // Usually your GitHub org/user name.
-  projectName: 'pairlegal_documentation', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "rooka-ai", // Usually your GitHub org/user name.
+  projectName: "pairlegal_documentation", // Usually your repo name.
+  deploymentBranch: "gh-pages",
   trailingSlash: true,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', "de"],
+    defaultLocale: "en",
+    locales: ["en", "de"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
   ],
+  plugins: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
 
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      },
+    ],
+  ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       colorMode: {
-        defaultMode: 'light',
+        defaultMode: "light",
         disableSwitch: false,
         respectPrefersColorScheme: true,
       },
-      image: 'img/Logo.svg',
+      image: "img/Logo.svg",
       navbar: {
         logo: {
-          alt: 'Rooka Logo',
-          src: 'img/logo.svg',
-          srcDark: 'img/logo_dark.svg'
+          alt: "Rooka Logo",
+          src: "img/logo.svg",
+          srcDark: "img/logo_dark.svg",
         },
         items: [
           {
-            type: 'localeDropdown',
-            position: 'right',
+            type: "localeDropdown",
+            position: "right",
           },
           {
-            href: 'https://www.rooka.ai',
-            label: 'About us',
-            position: 'right',
+            href: "https://www.rooka.ai",
+            label: "About us",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
-        links: [
-        ],
+        style: "dark",
+        links: [],
         copyright: `Rooka Labs`,
       },
       prism: {
