3b4631175bc24fdca6145af014389e45e3e7f8dc
Karim S <karimsehimi3s1@gmail.com>
2024-01-11 10:03:58 +0100

    Update url

--- pairlegal-documentation/docusaurus.config.js
+++ pairlegal-documentation/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
 
   // Set the production url of your site here
 
-  url: "https://rooka.ai/", //Shouldn't be https://docs.singledraft.ai
+  url: "https://docs.singledraft.ai", //Shouldn't be https://docs.singledraft.ai
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
