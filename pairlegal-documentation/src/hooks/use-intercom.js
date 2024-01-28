245a561af313878d4a26c4373fad71b3ec1a1d2b
Denis <trzeciokdenis@gmail.com>
2024-01-28 22:22:02 +0100

    #359 - Added Intercom integratoin

---
+++ pairlegal-documentation/src/hooks/use-intercom.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import queryString from 'query-string';
+import { Redirect, useLocation } from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+const useIntercom = () => {
+  const [intercomShown, setIntercomShown] = useState(false);
+  const location = useLocation();
+  const queryParams = queryString.parse(location.search);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' &&
+      queryParams.intercom &&
+      !intercomShown
+    ) {
+      const intercomDataJson = atob(queryParams.intercom);
+      const intercomData = JSON.parse(intercomDataJson);
+
+      console.log(intercomData);
+
+      window.Intercom = window.Intercom || function () {
+        window.Intercom.q.push(arguments);
+      };
+      window.Intercom.q = window.Intercom.q || [];
+      window.Intercom('boot', { app_id: intercomData.AppId });
+
+      // Insert the Intercom script into the <head>
+      const ic = document.createElement('script');
+      ic.type = 'text/javascript';
+      ic.async = true;
+      ic.src = `https://widget.intercom.io/widget/${intercomData.appId}`;
+      ic.onload = () => {
+        window.Intercom('boot', {
+          app_id: intercomData.AppId,
+          email: intercomData.Username,
+          user_hash: intercomData.HashedUsername,
+        });
+        window.Intercom('show');
+
+        setIntercomShown(true);
+      };
+
+      const head = document.head || document.getElementsByTagName('head')[0];
+      head.appendChild(ic);
+    }
+  }, [queryParams]);
+};
+
+export default useIntercom;
--- pairlegal-documentation/src/pages/index.js
+++ pairlegal-documentation/src/pages/index.js
@@ -1,6 +1,8 @@
 import { Redirect } from '@docusaurus/router';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import useIntercom from '../hooks/use-intercom';
 
 export default function Home() {
+  useIntercom();
   return <Redirect to={useBaseUrl("/docs/getting_started/")} />;
 };
